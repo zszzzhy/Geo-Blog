b6026a6ff41093af94a7dfbd1c0b79034992aafd
zhanghaiying <zszzzhy@dingtalk.com>
2019-06-26 23:49:06 +0800

    '项目初始化'

--- src/main.js
+++ src/main.js
@@ -3,10 +3,26 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-Vue.config.productionTip = false;
+import * as filters from "./filters";
+import VueFetch, { $fetch } from "./plugins/fetch";
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount("#app");
+// 过滤器
+for (const key in filters) {
+  Vue.filter(key, filters[key]);
+}
+
+// API域名配置
+Vue.use(VueFetch, {
+  baseUrl: "http://localhost:3000"
+});
+
+function main() {
+  // 启动应用
+  new Vue({
+    router,
+    store,
+    render: h => h(App)
+  }).$mount("#app");
+}
+
+main();
